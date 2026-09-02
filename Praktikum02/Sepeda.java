926a9b8491184ec7053b17f878eb74ec36e56035
Fara Novanza <faranvzz@gmail.com>
2026-09-02 19:03:42 +0700

    Mendeklarasi constructor berparameter

--- Praktikum02/Sepeda.java
+++ Praktikum02/Sepeda.java
@@ -4,18 +4,17 @@ public class Sepeda {
     float kecepatan;
     int gear;
 
+    public Sepeda(float newKecepatan, int newGear) {
+        kecepatan = newKecepatan;
+        gear = newGear;
+    }
+
     public void tambahKecepatan(float increment) {
         kecepatan += increment;
-        if (kecepatan > 20) {
-            kecepatan = 20;
-        }
     }
 
     public void kurangiKecepatan(float decrement) {
         kecepatan -= decrement;
-        if (kecepatan < 0) {
-            kecepatan = 0;
-        }
     }
 
     public void cetakInfo() {
